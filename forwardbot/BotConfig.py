0553c864a1c78172665173ab8e41b8e497d438b3
Vk602504 <vikashkumarjalpura4@gmail.com>
2024-12-16 09:03:32 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = environ.get("API_ID", "20204401")
-    API_HASH = environ.get("API_HASH", "6ec374e7e7492de06a9b22bb746bd20f")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6922036916:AAHod6WEiUf70eq6ytG2P1uRrk6__uQ-xHU")
+    API_ID = environ.get("API_ID", "20838202")
+    API_HASH = environ.get("API_HASH", "fde3e78e3256ae13e868b39088c83838")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7816178665:AAFif8TNdDhH7cro5bVGgAFdkb25kjLIPL0")
     STRING_SESSION = environ.get("STRING_SESSION", "")
-    SUDO_USERS = environ.get("SUDO_USERS", "6168162777 6366990600")
+    SUDO_USERS = environ.get("SUDO_USERS", "5387859888")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
@@ -26,5 +26,5 @@ class Config(object):
     🔻 **Command :** /uptime
     **Usage :** Check uptime of Bot.
     
-    ⭕ **ʙᴏᴛ ɪs ᴄʀᴇᴀᴛᴇᴅ ʙʏ** **@KingVJ01**
+    ⭕ **ʙᴏᴛ ɪs ᴄʀᴇᴀᴛᴇᴅ ʙʏ** **ꪜᎥᴋꫝꜱʜ KᴀꜱʜʏᴀᎮ**
     """
